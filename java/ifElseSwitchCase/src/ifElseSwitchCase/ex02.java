0920ef72d4891f2f26b00f640f84c049bd64eda4
johnnyalvesx <alvesjohnny@outlook.com>
2021-10-03 22:21:43 -0300

    Revisão de exercícios IfElse - SwitchCase

---
+++ java/ifElseSwitchCase/src/ifElseSwitchCase/ex02.java
@@ -0,0 +1,20 @@
+package ifElseSwitchCase;
+
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com um número:");
+		int num = scan.nextInt();
+
+		if (num >= 0) {
+			System.out.println("O número informado é positivo");
+		} else {
+			System.out.println("O número informado é negativo");
+		}
+	}
+}
---
+++ java/ifElseSwitchCase/src/ifElseSwitchCase/ex18.java
@@ -0,0 +1,20 @@
+package ifElseSwitchCase;
+
+import java.util.Scanner;
+
+public class ex18 {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Entre com um número");
+		int num = scan.nextInt();
+
+		if (num % 2 == 0) {
+			System.out.println("par");
+		} else {
+			System.out.println("ímpar");
+		}
+	}
+}
